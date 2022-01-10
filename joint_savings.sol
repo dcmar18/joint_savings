0331a6e796f4b82e34f978ad3872d58e769853bc
dcmar18 <dcmar18@gmail.com>
2022-01-09 20:31:16 -0800

    Wrote require statemnt to check is recipient account is eaul to account1 or account 2. Also wrote code to check for sufficient funds in account

--- joint_savings.sol
+++ joint_savings.sol
@@ -42,12 +42,14 @@ contract JointSavings {
         /*
         Define a `require` statement that checks if the `recipient` is equal to either `accountOne` or `accountTwo`. The `requiere` statement returns the text `"You don't own this account!"` if it does not.
         */
-        // YOUR CODE HERE!
+ 
         require(recipient == account1 || recipient == account2, "You don't own this account!");
+ 
         /*
         Define a `require` statement that checks if the `balance` is sufficient to accomplish the withdraw operation. If there are insufficient funds, the text `Insufficient funds!` is returned.
         */
-        // YOUR CODE HERE!
+ 
+        require(address(this).balance >= amount), "Insufficient funds!")
 
         /*
         Add and `if` statement to check if the `lastToWithdraw` is not equal to (`!=`) to `recipient` If `lastToWithdraw` is not equal, then set it to the current value of `recipient`.
